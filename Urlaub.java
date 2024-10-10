1ab9bfb46d22214cb9953b26e2dfc4753fb11339
Armina Salcinovic <sal240766@spengergasse.at>
2024-10-10 21:28:31 +0200

    MultipleConstructors erstellt

--- Urlaub.java
+++ Urlaub.java
@@ -1,55 +1,75 @@
 
 public class Urlaub
 {
-    private String ort;
-    private int preis;
-    private boolean allInclusive;
-    
+    private String ort = "Wien";
+    private int preis = 1999;
+    private boolean allInclusive = false;
+
     public Urlaub(String ort, int preis, boolean allInclusive)
     {
         setOrt(ort);
         setPreis(preis);
         setAllInclusive(allInclusive);
     }
-    
-    
-    public void setOrt(String ort)
+
+    public Urlaub(String ort, int preis)
+    {
+        setOrt(ort);
+        setPreis(preis);
+        setAllInclusive(false);
+
+    }
+
+    public Urlaub(String ort)
+    {
+        setOrt(ort);
+        setPreis(1999);
+        setAllInclusive(false);
+    }
+
+    public Urlaub()
+    {
+       setOrt("Wien");
+       setPreis(1999);
+       setAllInclusive(false);
+    }
+
+     public void setOrt(String ort)
     {
         this.ort = ort;
     }
-    
+
     public void setPreis(int preis)
     {
         this.preis = preis;
-        
+
     }
-    
+
     public void setAllInclusive(boolean allInclusive)
     {
         this.allInclusive = allInclusive;
     }
-    
+
     public String getOrt()
     {
         return ort;
 
     }
-    
+
     public int getPreis()
     {
         return preis;
     }
-    
+
     public boolean getAllInclusive()
     {
         return allInclusive;
     }
-    
+
     public void printUrlaub()
     {
         System.out.println(ort + " - " + preis + " EUR - AllInclusive: " + allInclusive);
     }
-    
-    
+
     
 }
